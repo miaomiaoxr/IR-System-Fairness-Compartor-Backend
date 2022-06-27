909d021b9e702a41bfea35ba3418439bcf3a4a8c
miaomiaoxr <miaomiaoxr1234@gmail.com>
2022-06-27 17:05:42 +0100

    add logger & enable cors

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
+app.use(express.json());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));//request logger
+app.use(cors());//enable cors
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
